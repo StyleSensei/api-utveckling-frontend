670058c768aa31288976df82e945ba9a5c050331
Patrik Arell <patrikarell@gmail.com>
2024-03-22 14:03:45 +0100

    removed duplicate fetch

--- createPager.js
+++ createPager.js
@@ -1,32 +1,32 @@
-import { refresh } from './script.js';
-let currentPageNo = 1;
-function setCurrentPageNo(newPageNo) {
-  currentPageNo = newPageNo;
-}
+// import { refresh } from './script.js';
+// let currentPageNo = 1;
+// function setCurrentPageNo(newPageNo) {
+//   currentPageNo = newPageNo;
+// }
 
-function createPager(count, pageNo, currentPageSize) {
-  const pager = document.getElementById('pager');
+// function createPager(count, pageNo, currentPageSize) {
+//   const pager = document.getElementById('pager');
 
-  pager.innerHTML = '';
-   let totalPages = Math.ceil(count / currentPageSize);
-  for (let i = 1; i <= totalPages; i++) {
-    const li = document.createElement('li');
-    li.classList.add('page-item');
-    if (i == pageNo) {
-      li.classList.add('active');
-    }
-    const a = document.createElement('a');
-    a.href = '#';
-    a.innerText = i;
-    a.classList.add('page-link');
-    li.appendChild(a);
-    a.addEventListener('click', (e) => {
-      // document.getElementsByClassName('container')[1].scrollIntoView();
-      e.preventDefault();
-      currentPageNo = i;
-      refresh();
-    });
-    pager.appendChild(li);
-  }
-}
-export { createPager, setCurrentPageNo}
+//   pager.innerHTML = '';
+//    let totalPages = Math.ceil(count / currentPageSize);
+//   for (let i = 1; i <= totalPages; i++) {
+//     const li = document.createElement('li');
+//     li.classList.add('page-item');
+//     if (i == pageNo) {
+//       li.classList.add('active');
+//     }
+//     const a = document.createElement('a');
+//     a.href = '#';
+//     a.innerText = i;
+//     a.classList.add('page-link');
+//     li.appendChild(a);
+//     a.addEventListener('click', (e) => {
+//       // document.getElementsByClassName('container')[1].scrollIntoView();
+//       e.preventDefault();
+//       currentPageNo = i;
+//       refresh();
+//     });
+//     pager.appendChild(li);
+//   }
+// }
+// export { createPager, setCurrentPageNo}
--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function createPager(count, pageNo, currentPageSize) {
   const pager = document.getElementById('pager');
 
   pager.innerHTML = '';
-   let totalPages = Math.ceil(count / currentPageSize);
+  let totalPages = Math.ceil(count / currentPageSize);
   for (let i = 1; i <= totalPages; i++) {
     const li = document.createElement('li');
     li.classList.add('page-item');
@@ -80,13 +80,11 @@ function createPager(count, pageNo, currentPageSize) {
   }
 }
 
-
 const updateQuery = debounce((query) => {
   currentQ = query;
   currentPageNo = 1;
   refresh();
   overlay.style.display = 'none';
-  setTimeout(() => {}, 500);
 }, 500);
 
 searchPlayer.addEventListener('input', (e) => {
@@ -120,14 +118,8 @@ searchPlayer.addEventListener('input', function () {
       players[i].visible = false;
     }
   }
-  refresh();
 });
 
-// const createTableTdOrTh = function (elementType, innerText) {
-//   let element = document.createElement(elementType);
-//   element.textContent = innerText;
-//   return element;
-// };
 
 const onClickPlayer = function (event) {
   const clickedElement = event.target;
@@ -217,7 +209,6 @@ function createTd(data) {
 
 export async function refresh() {
   let offset = (currentPageNo - 1) * currentPageSize;
-  //fetch!
   let url =
     'http://localhost:3000/api/players?sortCol=' +
     currentSortCol +
@@ -242,32 +233,32 @@ export async function refresh() {
     allPlayersTBody.innerHTML = '';
     createPager(response.total, currentPageNo, currentPageSize);
     createResponseMessage404();
+  } else {
+    const players = await response.json();
+
+    allPlayersTBody.innerHTML = '';
+
+    players.result.forEach((player) => {
+      const tr = document.createElement('tr');
+      tr.appendChild(createTd(player.name));
+      tr.appendChild(createTd(player.jersey));
+      tr.appendChild(createTd(player.position));
+      tr.appendChild(createTd(player.team));
+
+      const td = document.createElement('td');
+      const btn = document.createElement('button');
+      btn.classList.add('btn', 'btn-secondary', 'btn-sm');
+      btn.textContent = 'EDIT';
+      btn.dataset.playerid = player.id;
+      td.appendChild(btn);
+      tr.appendChild(td);
+
+      btn.addEventListener('click', onClickPlayer);
+
+      allPlayersTBody.appendChild(tr);
+    });
+    createPager(players.total, currentPageNo, currentPageSize);
   }
-
-  const players = await response.json();
-
-  allPlayersTBody.innerHTML = '';
-
-  players.result.forEach((player) => {
-    const tr = document.createElement('tr');
-    tr.appendChild(createTd(player.name));
-    tr.appendChild(createTd(player.jersey));
-    tr.appendChild(createTd(player.position));
-    tr.appendChild(createTd(player.team));
-
-    const td = document.createElement('td');
-    const btn = document.createElement('button');
-    btn.classList.add('btn', 'btn-secondary', 'btn-sm');
-    btn.textContent = 'EDIT';
-    btn.dataset.playerid = player.id;
-    td.appendChild(btn);
-    tr.appendChild(td);
-
-    btn.addEventListener('click', onClickPlayer);
-
-    allPlayersTBody.appendChild(tr);
-  });
-  createPager(players.total, currentPageNo, currentPageSize);
 }
 
 await refresh();
